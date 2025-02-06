ca17ccb5ea105c3fd2145591d3702848a3cbe0c9
Ryan Paul <ryan.r.paul22@gmail.com>
2025-02-06 02:19:57 -0500

    decreased text size throughout the site

--- components/hero.tsx
+++ components/hero.tsx
@@ -36,8 +36,8 @@ export default function Hero() {
                 className="rounded-lg shadow-lg max-w-full h-auto"
               />
             </div>
-            <p className="text-xl text-gray-400 mb-8 " data-aos="fade-up" data-aos-delay="200">Our foundation seeks to commemorate Gabrielle Kraft Buckman’s unwavering spirit of generosity and love. Gabby, known affectionately as Goober, Goob, Gabs, devoted her life to uplifting others and leaving an indelible mark of selflessness.</p>
-            <p className="text-xl text-gray-400 mb-8 " data-aos="fade-up" data-aos-delay="200">Gabrielle’s profound impact on countless lives, both intimately known and briefly encountered, is a testament to the depth of her compassion. The foundation stands as a tribute to Gabby’s boundless love, aiming to continue the legacy of kindness she embodied and the passion she had to give to those who gave.</p>
+            <p className="text-lg text-gray-400 mb-8 " data-aos="fade-up" data-aos-delay="200">Our foundation seeks to commemorate Gabrielle Kraft Buckman’s unwavering spirit of generosity and love. Gabby, known affectionately as Goober, Goob, Gabs, devoted her life to uplifting others and leaving an indelible mark of selflessness.</p>
+            <p className="text-lg text-gray-400 mb-8 " data-aos="fade-up" data-aos-delay="200">Gabrielle’s profound impact on countless lives, both intimately known and briefly encountered, is a testament to the depth of her compassion. The foundation stands as a tribute to Gabby’s boundless love, aiming to continue the legacy of kindness she embodied and the passion she had to give to those who gave.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
                 <a
